5eb24ff2698badad43c8aa70a5883ef380797d5f
Mrhelado <74932278+Helairgoandruosqui@users.noreply.github.com>
2025-05-02 10:56:50 -0300

    Update Tu casa bandera party!!!.py
    progreso

--- Tu casa bandera party!!!.py
+++ Tu casa bandera party!!!.py
@@ -3,9 +3,12 @@ import imaplib
 posR = 1
 posB = 50
 while True:
-    if(posR>=196 or posB >=196):
+    if(posR>=196):
+       posR=50
+    
+    if(posB >=196):
        break
- 
+    
     dado= random.randint (1,6)
     dado2 = random.randint (1,6)
     DadoT = dado + dado2
@@ -16,3 +19,9 @@ while True:
     DadoT = dado + dado2
     posB += DadoT
     print(f"A:{posR} B:{posB}")
+
+
+    if posR == 12:
+        print("bloqueo")
+    if posR == 151:
+        print("bloqueo")
