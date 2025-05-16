9a4b2096616d2ea81d1e2bc957668884ca626f0f
Mrhelado <74932278+Helairgoandruosqui@users.noreply.github.com>
2025-05-16 11:51:49 -0300

    Update Tu casa bandera party!!!.py
    New nos

--- Tu casa bandera party!!!.py
+++ Tu casa bandera party!!!.py
@@ -2,27 +2,30 @@ import random
 import imaplib
 posR = 1
 posB = 50
+
+def tirarDados():
+    dado= random.randint (1,6)
+    dado2 = random.randint (1,6)
+    DadoT = dado + dado2   
+    return DadoT
+
 while True:
     if(posR>=196):
        posR=50
     if(posB >=1000):
        break
     
-    dado= random.randint (1,6)
-    dado2 = random.randint (1,6)
-    DadoT = dado + dado2
-    posR += DadoT
+
+    posR += tirarDados()
 
     dado= random.randint (1,6)
     dado2 = random.randint (1,6)
     DadoT = dado + dado2
     posB += DadoT
-    print (f"A:{posR} B:{posB}")
+    print(f"A:{posR} B:{posB}")
 
 
     if posR == 12:
-        print("bloqueo")
-    if posR == 151:
         print("bloqueo")
     if posR == 166:
         print("bloqueo")
@@ -34,10 +37,11 @@ while True:
         posR = 50+(posR-196)
 
     if posR==151:
-        print =input("Seguir adelande o cambiar de dirección")
-    if (posR == "Seguir adelante"):
-        posR+0
-    if (posR == "Cambiar de dirección"):
-        posR-134 and posB-134
-    
-        
+        direc =input("Seguir adelande o cambiar de dirección")
+        if (direc == "S" or direc == "Seguir adelante"):
+            pass
+        if (direc == "N" or direc == "Cambiar de dirección"):
+            posR=17
+            print (f"A:{17} B:{20}")
+
+
